4b1fb8634e5f7231898cbd43d1f61e500cfbb5bf
VAISAGH MP <122623129+vaisagh-mp@users.noreply.github.com>
2023-11-29 17:36:10 +0530

    Delete api directory

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-from rest_framework.routers import DefaultRouter
-
-
-router=DefaultRouter()
-router.register('api/vendor',VendorViewset,basename='vendor')
-router.register('api/purchase_orders',PurchaseOrderViewset,basename='purchase_orders')
-urlpatterns =router.urls
